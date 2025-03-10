13c2cba85f58348b355b6f77d8bb244c231eb381
AveeBees <augustofvioseric@gmail.com>
2025-03-10 07:55:47 +0700

    chalk dan yargs

--- coba.js
+++
@@ -1,7 +0,0 @@
-// console.log("Hallo World!");
-
-function cetaknama (nama) {
-    return `Hello world saya ${nama}`;
-}
-
-module.exports = cetaknama;
--- index.js
+++
@@ -1,5 +0,0 @@
-// require('./coba.js')
-// console.log("Hello PPLG");
-
-const cetaknama = require('./coba.js');
-console.log(cetaknama('Eric'));
